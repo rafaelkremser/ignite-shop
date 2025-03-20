958cd80e9ddf5e3188a7059361b17dcd29323350
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2025-03-20 14:39:30 -0300

    feat: add product images and implement home page layout with styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,15 @@ import { globalStyles } from '../styles/global'
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '@/styles/pages/app'
 
+import Image from 'next/image'
+
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} width={130} height={52} alt="" />
       </Header>
 
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,28 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Testedassd</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1.src} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2.src} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
