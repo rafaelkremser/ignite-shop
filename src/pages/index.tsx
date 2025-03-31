3e5c05fe8cbac9b1dd6dc93be5e2fefcdc2d0a92
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2025-03-31 13:38:51 -0300

    feat: switch from server-side to static generation for home page and add revalidation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
-
-import { useKeenSlider } from 'keen-slider/react'
 
 import { HomeContainer, Product } from '../styles/pages/home'
 
+import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '@/lib/stripe'
-import { GetServerSideProps } from 'next'
 import Stripe from 'stripe'
 
 interface HomeProps {
@@ -54,7 +53,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   })
@@ -72,5 +71,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { products },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
